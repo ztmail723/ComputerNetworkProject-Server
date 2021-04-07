6b66d23408a5651d0440c17da721fd8264c5c04b
ztmail723 <ztmail723@gmail.com>
2021-04-07 19:39:54 +0800

    test2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <winsock2.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
+    sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
