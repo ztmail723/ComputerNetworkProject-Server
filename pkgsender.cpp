b9088afa3a78c98450970ed8c952ae56717924c9
ztmail723@gmail.com <44718750+ztmail723@users.noreply.github.com>
2021-04-23 22:18:22 +0800

    完善了三个sender函数

--- pkgsender.cpp
+++ pkgsender.cpp
@@ -5,3 +5,37 @@ PkgSender::PkgSender(MyTcpSocket* parent)
     , socket(parent)
 {
 }
+
+void PkgSender::send2001(QVector<QVariant> fileList)
+{
+    DataPkg pkg;
+    pkg.ID = 2001;
+    pkg.data = fileList;
+    send(pkg);
+}
+
+void PkgSender::send2002(QString fileName, quint64 fileCount)
+{
+    DataPkg pkg;
+    pkg.ID = 2002;
+    pkg.data.append(fileName);
+    pkg.data.append(fileCount);
+    send(pkg);
+}
+
+void PkgSender::send2003(quint64 fileID, QByteArray fileData)
+{
+    DataPkg pkg;
+    pkg.ID = 2003;
+    pkg.data.append(fileID);
+    pkg.data.append(fileData);
+    send(pkg);
+}
+
+void PkgSender::send(DataPkg& pkg)
+{
+    QByteArray arr;
+    QDataStream dts(&arr, QIODevice::WriteOnly);
+    dts << pkg;
+    this->socket->write(arr);
+}
--- pkgsender.h
+++ pkgsender.h
@@ -1,6 +1,7 @@
 #ifndef PKGSENDER_H
 #define PKGSENDER_H
 
+#include "datapkg.h"
 #include <QObject>
 class MyTcpSocket;
 class PkgSender : public QObject {
@@ -8,6 +9,14 @@ class PkgSender : public QObject {
 public:
     explicit PkgSender(MyTcpSocket* parent);
     MyTcpSocket* socket;
+
+public:
+    void send2001(QVector<QVariant> fileList);
+    void send2002(QString fileName, quint64 fileCount);
+    void send2003(quint64 fileID, QByteArray fileData);
+
+private:
+    void send(DataPkg& pkg);
 signals:
 };
 
