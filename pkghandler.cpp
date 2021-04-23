915029b2498309add94aa494e71b6a52818d413e
ztmail723@gmail.com <44718750+ztmail723@users.noreply.github.com>
2021-04-23 22:18:44 +0800

    完善了handler函数

--- pkghandler.cpp
+++ pkghandler.cpp
@@ -1,9 +1,13 @@
 #include "pkghandler.h"
 #include "mytcpsocket.h"
+#include "pkgsender.h"
+#include <QDir>
+#include <QVariant>
 PkgHandler::PkgHandler(MyTcpSocket* parent)
     : QObject(parent)
     , socket(parent)
 {
+    connect(this, &PkgHandler::printMsg, socket, &MyTcpSocket::printTextToWindow);
 }
 
 void PkgHandler::handle(DataPkg& pkg)
@@ -11,10 +15,36 @@ void PkgHandler::handle(DataPkg& pkg)
     quint64 ID = pkg.ID;
     switch (ID) {
     case 1001:
+        handle1001(pkg);
         break;
     case 1002:
+        handle1002(pkg);
+        break;
+    case 1003:
+        handle1003(pkg);
         break;
     default:
         break;
     }
+}
+
+void PkgHandler::handle1001(DataPkg& pkg)
+{
+    QString userName = pkg.data.at(0).toString();
+    emit printMsg("用户" + userName + "请求获取文件列表");
+    QDir dirPath("file");
+    QVector<QVariant> list;
+    QList<QFileInfo> file(dirPath.entryInfoList());
+    for (auto& i : file) {
+        list.append(i.fileName());
+    }
+    this->socket->sender->send2001(list);
+}
+
+void PkgHandler::handle1002(DataPkg& pkg)
+{
+}
+
+void PkgHandler::handle1003(DataPkg& pkg)
+{
 }
--- pkghandler.h
+++ pkghandler.h
@@ -10,7 +10,13 @@ public:
     explicit PkgHandler(MyTcpSocket* parent);
     MyTcpSocket* socket;
     void handle(DataPkg& pkg);
+
+private:
+    void handle1001(DataPkg& pkg);
+    void handle1002(DataPkg& pkg);
+    void handle1003(DataPkg& pkg);
 signals:
+    void printMsg(QString msg);
 };
 
 #endif // PKGHANDLER_H
